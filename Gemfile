c51552b39cc1aa91f31518147d47eda56407e82e
Mike Calhoun <mcalhoun@centresource.com>
2012-05-05 13:25:22 -0500

    adding some error checking

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem "rmagick", :require => 'RMagick'
 
 #gem 'pg'
 gem 'oink'
-
+gem 'pry'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,9 +5,18 @@ class PagesController < ApplicationController
 
   def show
     corgis = ["corgi1.jpg", "corgi2.jpg", "corgi3.jpeg"]
-    img = Magick::Image::read("http://michaelcalhoun.com/corgis/" + corgis.sample).first
-    #img = Magick::Image::read("http://imgs.sfgate.com/blogs/images/sfgate/pets/2010/07/23/pembroke_welsh_corgi400x313.jpg").first
-    resized = img.resize_to_fill(params[:width].to_i,params[:height].to_i)
-    send_data resized.to_blob, :type => 'image/jpg',:disposition => 'inline'
+    
+    height = params[:height].to_i
+    width  = params[:width].to_i
+    if (width > 0) && (height > 0)
+      if width <= 1000 && height <= 1000
+        img = Magick::Image::read("http://michaelcalhoun.com/corgis/" + corgis.sample).first
+        resized = img.resize_to_fill(width,height)
+        send_data resized.to_blob, :type => 'image/jpg',:disposition => 'inline'
+      else
+        img = Magick::Image::read("http://michaelcalhoun.com/corgis/" + corgis.sample).first
+        send_data img.to_blob, :type => 'image/jpg',:disposition => 'inline'
+      end
+    end
   end
 end
