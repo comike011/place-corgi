414726836d4abadb175ba039abd83c68308a5f68
Mike Calhoun <mcalhoun@centresource.com>
2012-05-05 12:08:28 -0500

    making it work really well with remote image hosting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,10 @@ class PagesController < ApplicationController
   end
 
   def show
-    img = Magick::Image::read("http://imgs.sfgate.com/blogs/images/sfgate/pets/2010/07/23/pembroke_welsh_corgi400x313.jpg").first
-    resized = img.resize_to_fit(params[:width],params[:height])
+    corgis = ["corgi1.jpg", "corgi2.jpg", "corgi3.jpeg"]
+    img = Magick::Image::read("http://michaelcalhoun.com/corgis/" + corgis.sample).first
+    #img = Magick::Image::read("http://imgs.sfgate.com/blogs/images/sfgate/pets/2010/07/23/pembroke_welsh_corgi400x313.jpg").first
+    resized = img.resize_to_fill(params[:width].to_i,params[:height].to_i)
     send_data resized.to_blob, :type => 'image/jpg',:disposition => 'inline'
   end
 end
