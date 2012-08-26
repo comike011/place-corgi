94e87efa8eb6b58d36bfa7edfb4262b103342f18
Travis Roberts <traviskroberts@gmail.com>
2012-08-26 13:12:39 -0500

    Added basic styling of homepage.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 PlaceCorgi::Application.routes.draw do
-  match "/:width/:height", :to => 'pages#show'
-  match "/v/:width/:height", :to => 'pages#video'
+  match "/:width/:height", :to => 'pages#show', :as => :corgi
+  match "/v/:width/:height", :to => 'pages#video', :as => :corgi_video
   root :to => 'pages#index'
 end
