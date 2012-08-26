402a2d551d87bfa90fc4397b6b3cfc51bb41453c
Travis Roberts <traviskroberts@gmail.com>
2012-08-26 11:15:47 -0500

    Updated project to use images on disk.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,30 +1,29 @@
 class PagesController < ApplicationController
   def index
-    
+
   end
 
   def show
-    corgis = ["corgi1.jpg", "corgi2.jpg", "corgi3.jpeg", "corgi4.jpg"]
-    
-    height = params[:height].to_i
+    # heroku varnish caching
+    response.headers['Cache-Control'] = 'public, max-age=28800' # 8 hours
+
     width  = params[:width].to_i
+    height = params[:height].to_i
+
     if (width > 0) && (height > 0)
-      if width <= 1000 && height <= 1000
-        img = Magick::Image::read("http://michaelcalhoun.com/corgis/" + corgis.sample).first
-        resized = img.resize_to_fill(width,height)
-        send_data resized.to_blob, :type => 'image/jpg',:disposition => 'inline'
-      else
-        img = Magick::Image::read("http://michaelcalhoun.com/corgis/" + corgis.sample).first
-        send_data img.to_blob, :type => 'image/jpg',:disposition => 'inline'
-      end
+      file_path = Rails.root.join('public', 'corgis')
+      corgi_file = Dir.glob("#{file_path}/corgi*").sample
+      img = Magick::Image::read(corgi_file).first
+      final_image = img.resize_to_fill(width, height)
+
+      send_data final_image.to_blob, :type => 'image/jpg',:disposition => 'inline'
     end
   end
-  
+
   def video
     @height = params[:height].to_i
     @width  = params[:width].to_i
-    
-    
+
+
   end
-  
 end
