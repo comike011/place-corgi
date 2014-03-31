f614212398ffde9b7102501eee520e51b4bf37ec
Travis Roberts <traviskroberts@gmail.com>
2014-03-30 19:17:10 -0500

    Moved session secret to env variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PlaceCorgi::Application.config.secret_token = '73defa5b2be86243c2e9e32c889428ca34ae8701810a266686d8c42b66a94bfc596b72d5f813ff7363c0d53e2e9b9baed4cec3ebed89727ba735993af8137d61'
+PlaceCorgi::Application.config.secret_token = ENV['SECRET_TOKEN']
